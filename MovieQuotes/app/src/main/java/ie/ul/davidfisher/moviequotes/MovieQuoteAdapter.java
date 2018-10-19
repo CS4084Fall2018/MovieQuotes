d003be70fed046b6d02e927641f5ceb27ba0811b
David Fisher <fisherds@rose-hulman.edu>
2018-10-19 14:40:22 +0100

    passing an Intent Extra

--- MovieQuotes/app/src/main/java/ie/ul/davidfisher/moviequotes/MovieQuoteAdapter.java
+++ MovieQuotes/app/src/main/java/ie/ul/davidfisher/moviequotes/MovieQuoteAdapter.java
@@ -74,9 +74,11 @@ public class MovieQuoteAdapter extends RecyclerView.Adapter<MovieQuoteAdapter.Mo
       itemView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+          DocumentSnapshot ds = mMovieQuoteSnapshots.get(getAdapterPosition());
+
           Context c = view.getContext();
           Intent intent = new Intent(c, MovieQuoteDetailActivity.class);
-          
+          intent.putExtra(Constants.EXTRA_DOC_ID, ds.getId());
           c.startActivity(intent);
         }
       });
